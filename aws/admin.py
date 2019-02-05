776002196a3cf76d805095843edaf71d6e7caf83
Farhaan <patel.farhaaan@gmail.com>
2019-02-05 21:33:33 +0530

    sqlite3 added

--- aws/admin.py
+++ aws/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from aws.models import AppsDescription, InfraServiceInfo, ServerAwsInfo, StaticData
 
 
-admin.site.register(StaticData)
-admin.site.register(ServerAwsInfo)
 admin.site.register(AppsDescription)
+admin.site.register(StaticData)
 admin.site.register(InfraServiceInfo)
+admin.site.register(ServerAwsInfo)
